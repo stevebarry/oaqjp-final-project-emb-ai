90437bb8e41a5ea618cd173152fcfbb24d762928
Steve Barry <stevebarry1@gmail.com>
2025-07-24 15:58:09 -0400

    new file added

---
+++ emotion_detection.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+
+def emotion_detector(text_to_analyze):
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post( URL, headers=headers, data = json.dumps(input_json) )
+    emotions={}
+
+    r_dict = json.loads(response.text)
+
+    return_val = r_dict["emotionPredictions"][0]["emotion"]
+    return_val['dominant_emotion'] = max(return_val, key=return_val.get)
+
+    return return_val
+
+if __name__ == "__main__":
+    print( emotion_detector('I love this technology.') )
